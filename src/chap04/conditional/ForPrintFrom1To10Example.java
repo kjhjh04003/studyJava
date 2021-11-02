478d2eaf3d85f581029ce01fdd455b77085f20bd
kjhjh <kjhjh04003@naver.com>
2021-11-02 20:09:25 +0900

    반복문 - for, 중첩 for

---
+++ src/chap04/conditional/ForPrintFrom1To10Example.java
@@ -0,0 +1,10 @@
+package chap04.conditional;
+
+// 1부터 10까지 출력
+public class ForPrintFrom1To10Example {
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
+	}
+}
