0cb35adcea53643f2edacc5d4e85873ebbb6ff44
kjhjh <kjhjh04003@naver.com>
2021-11-02 16:48:26 +0900

    이항 연산자 - 비교 연산자

---
+++ src/chap03/operator/StringEqualsExample.java
@@ -0,0 +1,17 @@
+package chap03.operator;
+
+// πÆ¿⁄ø≠ ∫Ò±≥
+public class StringEqualsExample {
+	public static void main(String[] args) {
+		String strVar1 = "Ω≈πŒ√∂";
+		String strVar2 = "Ω≈πŒ√∂";
+		String strVar3 = new String("Ω≈πŒ√∂");
+		
+		System.out.println(strVar1 == strVar2);
+		System.out.println(strVar1 == strVar3);
+		System.out.println();
+		// equals() : String ∞¥√º¿« πÆ¿⁄ø≠∏∏ ∫Ò±≥«œ∞Ì ΩÕ¿ª ∂ß
+		System.out.println(strVar1.equals(strVar2));
+		System.out.println(strVar1.equals(strVar3));
+	}
+}
