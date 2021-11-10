66561b6f19815b8f14350b60d2dc7259cf34f9b6
kjhjh <kjhjh04003@naver.com>
2021-11-10 14:14:49 +0900

    클래스 구성멤버 - 필드, 필드초기값

---
+++ src/chap06/FieldInitValue.java
@@ -0,0 +1,19 @@
+package chap06;
+
+// 필드 자동 초기화
+public class FieldInitValue {
+	// 필드
+	byte byteField;
+	short shortField;
+	int intField;
+	long longField;
+
+	boolean booleanField;
+	char charField;
+
+	float floatField;
+	double doubleField;
+
+	int[] arrField;
+	String referenceField;
+}
